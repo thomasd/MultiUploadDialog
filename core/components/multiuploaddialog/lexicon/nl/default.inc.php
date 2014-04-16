feccd03252689c2ff88b9761c50930dc598322a1
DonQuicky <ivar.snel@gmail.com>
2014-04-17 01:02:45 +0200

    Create default.inc.php
    Add new lexicon file for Dutch language

---
+++ core/components/multiuploaddialog/lexicon/nl/default.inc.php
@@ -0,0 +1,51 @@
++	<?php
++	/**
++	 * nl default topic lexicon file for MultiUploadDialog extra
++	 *
++	 * Copyright 2014 by Ivar Snel [http://www.codeplaza.com]
++	 * Created on 04-17-2014
++	 *
++	 * MultiUploadDialog is free software; you can redistribute it and/or modify it under the
++	 * terms of the GNU General Public License as published by the Free Software
++	 * Foundation; either version 2 of the License, or (at your option) any later
++	 * version.
++	 *
++	 * MultiUploadDialog is distributed in the hope that it will be useful, but WITHOUT ANY
++	 * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
++	 * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
++	 *
++	 * You should have received a copy of the GNU General Public License along with
++	 * MultiUploadDialog; if not, write to the Free Software Foundation, Inc., 59 Temple
++	 * Place, Suite 330, Boston, MA 02111-1307 USA
++	 *
++	 * @package multiuploaddialog
++	 */
++	
++	/**
++	 * Description
++	 * -----------
++	 * nl default topic lexicon strings
++	 *
++	 * Variables
++	 * ---------
++	 * @var $modx modX
++	 * @var $scriptProperties array
++	 *
++	 * @package multiuploaddialog
++	 **/
++	
++	$_lang['multiuploaddialog.buttons.choose']           = 'Kies bestand(en)';
++	$_lang['multiuploaddialog.buttons.clear']            = 'Leeg lijst';
++	$_lang['multiuploaddialog.buttons.upload']           = 'Upload';
++	$_lang['multiuploaddialog.buttons.close']            = 'Sluiten';
++	$_lang['multiuploaddialog.title.destination_path']   = 'Upload bestemming: [[+path]]';
++	$_lang['multiuploaddialog.upload_progress']          = '[[+loaded]] / [[+total]]';
++	$_lang['multiuploaddialog.notpermitted.extension']   = 'Upload van dit bestandstype (.[[+ext]]) is niet toegestaan!';
++	$_lang['multiuploaddialog.notpermitted.filesize']    = 'Bestandsomvang groter ([[+size]]) dan toegestaan! (max. [[+max]])';
++	$_lang['multiuploaddialog.columns.file']             = 'Bestand';
++	$_lang['multiuploaddialog.columns.state']            = 'Status';
++	$_lang['multiuploaddialog.contextmenu.remove_entry'] = 'Verwijder invoer';
++	$_lang['multiuploaddialog.clear_list.all']           = 'Maak overzicht leeg';
++	$_lang['multiuploaddialog.clear_list.notpermitted']  = 'Verwijder alleen niet toegestaan';
++	$_lang['multiuploaddialog.msg.title.error']          = 'Fout';
++	$_lang['multiuploaddialog.upload.success']           = 'Upload succesvol';
