2b78fa388aafb4db87dbffbc63ef8952480a2cf0
dokutoku <rewaene@hotmail.co.jp>
2014-05-03 23:59:56 +0900

    Japanese translation

---
+++ core/components/multiuploaddialog/lexicon/ja/default.inc.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * ja default topic lexicon file for MultiUploadDialog extra
+ *
+ * Copyright 2014 by Thomas Dullnig
+ * Created on 02-27-2014
+ *
+ * MultiUploadDialog is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * MultiUploadDialog is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * MultiUploadDialog; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package multiuploaddialog
+ */
+
+/**
+ * Description
+ * -----------
+ * en default topic lexicon strings
+ *
+ * Variables
+ * ---------
+ * @var $modx modX
+ * @var $scriptProperties array
+ *
+ * @package multiuploaddialog
+ **/
+
+$_lang['multiuploaddialog.buttons.choose']           = 'ファイルを選択';
+$_lang['multiuploaddialog.buttons.clear']            = 'リストを空にする';
+$_lang['multiuploaddialog.buttons.upload']           = 'アップロード';
+$_lang['multiuploaddialog.buttons.close']            = '閉じる';
+$_lang['multiuploaddialog.title.destination_path']   = 'アップロード先: [[+path]]';
+$_lang['multiuploaddialog.upload_progress']          = '[[+loaded]] / [[+total]]';
+$_lang['multiuploaddialog.notpermitted.extension']   = 'このファイルタイプ (.[[+ext]]) のアップロードはできません！';
+$_lang['multiuploaddialog.notpermitted.filesize']    = 'ファイルサイズが ([[+size]]) を超えてはいけません！ (max. [[+max]])';
+$_lang['multiuploaddialog.columns.file']             = 'ファイル';
+$_lang['multiuploaddialog.columns.state']            = '状態';
+$_lang['multiuploaddialog.contextmenu.remove_entry'] = 'エントリーを除去';
+$_lang['multiuploaddialog.clear_list.all']           = '全てのリストをクリア';
+$_lang['multiuploaddialog.clear_list.notpermitted']  = '許可されないファイルのみを除去';
+$_lang['multiuploaddialog.msg.title.error']          = 'エラー';
+$_lang['multiuploaddialog.upload.success']           = 'アップロード成功';
