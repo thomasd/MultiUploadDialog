0abcb76ea7c0d13ad316776853e296375e372abc
Kristoffer Karlström <kristoffer@kmmtiming.se>
2014-11-08 19:58:35 +0100

    Update  default.inc.php
    Finished translation.

--- core/components/multiuploaddialog/lexicon/sv/ default.inc.php
+++ core/components/multiuploaddialog/lexicon/sv/ default.inc.php
@@ -1,2 +1,51 @@
 <?php
+/**
+ * sv default topic lexicon file for MultiUploadDialog extra
+ *
+ * Copyright 2014 by Kristoffer Karlström (https://github.com/kristk)
+ * Created on 11-08-2014
+ *
+ * MultiUploadDialog is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * MultiUploadDialog is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * MultiUploadDialog; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package multiuploaddialog
+ */
 
+/**
+ * Description
+ * -----------
+ * sv default topic lexicon strings
+ *
+ * Variables
+ * ---------
+ * @var $modx modX
+ * @var $scriptProperties array
+ *
+ * @package multiuploaddialog
+ **/
+
+$_lang['multiuploaddialog.buttons.choose']           = 'Välj fil(er)';
+$_lang['multiuploaddialog.buttons.clear']            = 'Töm listan';
+$_lang['multiuploaddialog.buttons.upload']           = 'Ladda upp';
+$_lang['multiuploaddialog.buttons.close']            = 'Stäng';
+$_lang['multiuploaddialog.title.destination_path']   = 'Sökväg: [[+path]]';
+$_lang['multiuploaddialog.upload_progress']          = '[[+loaded]] / [[+total]]';
+$_lang['multiuploaddialog.notpermitted.extension']   = 'Uppladdning av den här filtypen (.[[+ext]]) är inte tillåten!';
+$_lang['multiuploaddialog.notpermitted.filesize']    = 'Filstorleken är större ([[+size]]) än den tillåtna! (max. [[+max]])';
+$_lang['multiuploaddialog.columns.file']             = 'Fil';
+$_lang['multiuploaddialog.columns.state']            = 'Status';
+$_lang['multiuploaddialog.contextmenu.remove_entry'] = 'Ta bort raden';
+$_lang['multiuploaddialog.clear_list.all']           = 'Töm listan';
+$_lang['multiuploaddialog.clear_list.notpermitted']  = 'Ta bort ej tillåtna';
+$_lang['multiuploaddialog.msg.title.error']          = 'Fel';
+$_lang['multiuploaddialog.upload.success']           = 'Uppladdningen är slutförd';
